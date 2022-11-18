a3843abf9ed6006e0d41286fb423fa971d3ed219
muhitasraf <muhitasraf@gmail.com>
2022-11-18 23:02:29 +0600

    order product table added

---
+++ database/migrations/2022_11_18_225119_create_order_product_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->string('user_name',128);
+            $table->string('user_phone_number',32);
+            $table->timestamps();
+            $table->foreign('user_id')->reference('id')->on('user')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+    }
+}
