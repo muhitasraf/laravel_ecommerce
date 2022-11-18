1389a4ed78ffced3562c3b255393e08827ccb669
muhitasraf <muhitasraf@gmail.com>
2022-11-18 20:50:35 +0600

    User migration added

--- database/migrations/2022_11_18_142621_create_user_table.php
+++ database/migrations/2022_11_18_142621_create_user_table.php
@@ -15,6 +15,16 @@ class CreateUserTable extends Migration
     {
         Schema::create('user', function (Blueprint $table) {
             $table->id();
+            $table->string('name',128);
+            $table->string('eamil',128)->unique();
+            $table->string('phone_number',32)->unique();
+            $table->string('password',32);
+            $table->bigInteger('rewards')->default(0);
+            $table->tinyInteger('email_verified_at')->nullable();
+            $table->string('email_verified_token',100)->nullable();
+            $table->string('facebook_id',32)->nullable();
+            $table->string('google_id',32)->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
